29059d6692bc112d5d5360a940fe434a1a105681
Cristian Pintado <cpintado17@gmail.com>
2020-02-11 03:26:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
-def countdown_with_sleep(5.seconds)
+def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
   number -= 1 
